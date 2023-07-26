7d01b5e337a88621f6799c9a1ada9cd5d0df87ff
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-07-26 18:13:20 +1000

    defined shapes classes Triangle, Circle and Square

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const jest = require('jest');
 
-const generateSVG
+const {Square, Circle, Triangle} = require("./lib/shapes");
+
+const generateSVG = 
