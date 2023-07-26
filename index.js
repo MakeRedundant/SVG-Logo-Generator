1e08ee0b4c539a95a6f63993170fe7cc48148a15
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-07-26 20:40:19 +1000

    added promptSVG with inquirer

--- index.js
+++ index.js
@@ -6,4 +6,70 @@ const jest = require('jest');
 
 const {Triangle, Circle, Square} = require("./lib/shapes");
 
-const generateSVG = 
+
+
+
+//Inquirer function 
+ // Prompts the user with questions to render the SVG logo
+ const promptSVG = () => {
+    return inquirer
+      .prompt([
+        // Text prompt
+        {
+          type: "input",
+          message:
+            "What text would you like your logo to display? (Enter up to three characters)",
+          name: "text",
+          validate: (nameInput) =>
+            nameInput.length <= 3
+              ? true : "Please enter up to three characters", //Uses a conditional (ternary) operator to determine validation
+        },
+        // Text colour prompt
+        {
+          type: "input",
+          message: "Choose text color (Enter a color or a hexadecimal#)",
+          name: "textColor",
+          validate: (nameInput) =>
+            nameInput ? true : "Please enter a color for the text",
+        },
+        // Shapes choice prompt
+        {
+          type: "list",
+          message: "What shape would you like your logo to be?",
+          choices: ["Triangle", "Circle", "Square"],
+          name: "shape",
+          validate: (nameInput) =>
+            nameInput ? true : "Please select a shape for the logo",
+        },
+        // Shape colour prompt
+        {
+          type: "input",
+          message: "Choose shape color (Enter a color or a hexadecimal#)",
+          name: "shapeColour",
+          validate: (nameInput) =>
+            nameInput ? true : "Please enter a color for the shape",
+        },
+      ])
+      .then((answers) => {
+        // Calling write file function to generate SVG file
+        generateSVG("logo.svg", answers);
+      });
+  };
+
+//Initilise promptSVG function for user questions 
+  promptSVG();
+  
+//  GIVEN a command-line application that accepts user input
+// WHEN I am prompted for text
+// THEN I can enter up to three characters
+// WHEN I am prompted for the text color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I am prompted for a shape
+// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+// WHEN I am prompted for the shape's color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I have entered input for all the prompts
+// THEN an SVG file is created named `logo.svg`
+// AND the output text "Generated logo.svg" is printed in the command line
+// WHEN I open the `logo.svg` file in a browser
+// THEN I am shown a 300x200 pixel image that matches the criteria I entered
